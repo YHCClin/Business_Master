8176862e7dee9991512fb044074c1c155dbcf8f4
YHCClin <lyhdxzb@icloud.com>
2020-11-13 15:20:37 +0800

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: '',
+    loadChildren: () => import('./routes/guide/guide.module').then( m => m.GuidePageModule)
+  },
+  {
+    path: 'guide',
+    loadChildren: () => import('./routes/guide/guide.module').then( m => m.GuidePageModule)
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/routes/guide/guide.page.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlide, IonSlides } from '@ionic/angular';
+
+@Component({
+  selector: 'app-guide',
+  templateUrl: './guide.page.html',
+  styleUrls: ['./guide.page.scss'],
+})
+export class GuidePage implements OnInit {
+  showSkip = true;
+  @ViewChild('slides', {static: false}) slides: IonSlides;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSlideWillChange(event) {
+    this.slides.isEnd().then((end) => {
+      this.showSkip = !end;
+    });
+  }
+
+}
